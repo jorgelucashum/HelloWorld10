ce5c7aaf2770adbd2405b482d9f7b1fea3d993a4
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-15 16:33:18 -0300

    Add project files.

---
+++ HelloWorld10/Produto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization; // usado para converter 'CultureInfo.InvariantCulture' 
+
+namespace HelloWorld10
+{
+    //Sobrecarga: É um recurso que uma classe possui de oferecer mais de uma operação com o mesmo nome, porém com diferentes listas de parâmetros.
+    class Produto
+    {
+        public string Nome; //atributo com primeira letra maiusculo em classe fora da main
+        public int Quantidade;
+        public double Preco;
+
+        //Criando e definindo paramêtros no construtor:
+        public Produto(string nome, double preco, int quantidade) // Construtor tem o mesmo nome da classe
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco) // Sobrecarga de um construtor só com dois parâmetros de entrada, sendo possível coexistir o construtor de 3 e o de 2 parametros.
+        {
+            Nome = nome;
+            Preco = preco;
+            // Quantidade = 10; Pode ser definida a quantidade caso necessário, mas sem valor atribuido, por padrão é 0
+
+        }
+
+        public Produto() // Sobrecarga para funcionar o construtor padrão e sintaxe alternativa (Só é necessário quando se declara algum construtor)
+        {
+
+        }
+
+        public double ValorTotalEmEsqtoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade; // Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade; // Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString() // método usado para retornar a váriavel 'p1' nas informações em String
+        {
+            return Nome + ", R$:" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " " +
+                "Unidades, total R$:" + ValorTotalEmEsqtoque().ToString("F2", CultureInfo.InvariantCulture); // converter as ',' para '.' (padrão internacional)
+        }
+
+    }
+}
---
+++ HelloWorld10/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld10
+{
+    class Program
+    {
+        // Construtor: É uma operação especial da classe, que executa no momento da instanciação do objeto, servindo como um "molde". Uso comuns:
+        // - Iniciar valores dos atributos
+        // - Permitir ou obrigar que o objeto receba dados/dependências no momento de sua instanciação (injeção de dependência)
+        // - Se um construtor não for especificado dentro do parametro, a classe disponibiliza um contrutor padrão que recebe null e 0.0: Produto p = new Produto();
+        // - É possível especificar mais de um construtor na mesma classe (sobrecarga)
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o preço: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p1 = new Produto(nome, preco, quantidade); // Produto instanciado com os parametros obrigatorios do construtor
+
+            // Produto p2 = new Produto(nome, preco); // Também pode ser instanciado dessa forma com 2 parâmetros (construtor usando sobrecarga) 
+
+            // Produto p3 = new Produto(); // Construtor padrão também usando sobrecarga
+
+            // Produto p4 = new Produto() {Nome = nome, Preco = preco, Quantidade = quantidade}; // Sintaxe alternativa para iniciar valores igual ao construtor
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do pruduto: " + p1);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a quantidade de produtos a serem adicionadas");
+            int quant = int.Parse(Console.ReadLine());
+            p1.AdicionarProdutos(quant);
+            Console.WriteLine("Dados atualizados: " + p1);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a quantidade de produtos a serem removidas");
+            quant = int.Parse(Console.ReadLine());
+            p1.RemoverProdutos(quant);
+            Console.WriteLine("Dados atualizados: " + p1);
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
